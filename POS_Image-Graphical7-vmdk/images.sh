253392c5876f06e0bc1a5d8caa6c688c678e5cdb
pmullin99 <plmullin@gmail.com>
2025-12-05 09:28:04 +0800

    vmdk

--- POS_Image-Graphical7-vmdk/images.sh
+++ POS_Image-Graphical7-vmdk/images.sh
@@ -26,9 +26,9 @@ if [ -f /usr/bin/venv-salt-call ] ; then
   systemctl enable venv-salt-minion.service
 
   # notify SUSE Manager about newly deployed image
-systemctl enable image-deployed-bundle.service
+#systemctl enable image-deployed-bundle.service
 
-systemctl enable migrate-to-bundle.service
+#systemctl enable migrate-to-bundle.service
 
   # move the activation key injected by SUMA
 mv /etc/salt/minion.d/kiwi_activation_key.conf /etc/venv-salt-minion/minion.d
@@ -40,4 +40,4 @@ systemctl enable image-deployed.service
 fi
 
 # install bootloader and generate boot menu
-systemctl enable install-local-bootloader.service
+ systemctl enable install-local-bootloader.service
