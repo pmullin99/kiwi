1e8c392f9a1eddf688f8489ae86a6359da17e23f
pmullin99 <plmullin@gmail.com>
2025-12-15 10:34:02 +0800

    firstboot

---
+++ dmart/root/usr/share/firstboot/scripts/firstboot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm /etc/machine-id
+rm /var/lib/dbus/machine-id
+rm /var/lib/zypp/AnonymousUniqueId
+dbus-uuidgen --ensure
+systemd-machine-id-setup
+
+systemctl enable venv-salt-minion
