dd5a9af9f3ff3d4f7d90371c4d3b6ab77a598ee6
pmullin99 <plmullin@gmail.com>
2025-12-16 14:49:14 +0800

    sssd

--- post/sssd-setup-auto.sh
+++ post/sssd-setup-auto.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-rm /etc/machine-id
-rm /var/lib/dbus/machine-id
-rm /var/lib/zypp/AnonymousUniqueId
-dbus-uuidgen --ensure
-systemd-machine-id-setup
-
-systemctl enable --now venv-salt-minion
-
-#!/bin/bash
 #################################################################################################
 # sssd-setup-auto.sh
 # Author:       Patrick Mullin, November 2023
@@ -483,5 +473,4 @@ sleep 2
 ## Cleanup host file entry if required
 sed -i /"$HOST"/d /etc/hosts
 sed -i /"$AD_SERVER"/d /etc/hosts
-
 
