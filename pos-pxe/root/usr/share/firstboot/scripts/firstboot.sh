aecf6f23d6e244e2ed795ea263b93ab10892f9cf
pmullin99 <plmullin@gmail.com>
2026-06-19 16:12:59 +1000

    x

--- pos-pxe/root/usr/share/firstboot/scripts/firstboot.sh
+++ pos-pxe/root/usr/share/firstboot/scripts/firstboot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ## Reset machine ID
-rm /etc/machine-id
-rm /var/lib/dbus/machine-id
-rm /var/lib/zypp/AnonymousUniqueId
-dbus-uuidgen --ensure
-systemd-machine-id-setup
+#rm /etc/machine-id
+#rm /var/lib/dbus/machine-id
+#rm /var/lib/zypp/AnonymousUniqueId
+#dbus-uuidgen --ensure
+#systemd-machine-id-setup
 
 HOSTNAME=$(hostname -f)
 
@@ -14,4 +14,4 @@ echo $HOSTNAME > /etc/venv-salt-minion/minion_id
 ## Enable salt minion
 systemctl restart venv-salt-minion
 
-
+touch /root/first_boot_completed
